162d9ff3c91f28b49d651898fe452b14b485348d
Vincent Schneider <22986332+cli-ish@users.noreply.github.com>
2024-06-04 10:14:13 +0200

    bump version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_webshell';
-$plugin->version = 2024060200;
+$plugin->version = 2024060400;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.0';
+$plugin->release = '1.0.1';
 $plugin->supported = [311, 404];
