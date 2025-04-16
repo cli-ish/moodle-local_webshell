4c2b0717298c3ebb2adadb0d8e6239043eb144b6
Vincent Schneider <22986332+cli-ish@users.noreply.github.com>
2025-04-16 19:14:08 +0200

    removed 311 support

--- version.php
+++ version.php
@@ -29,4 +29,4 @@ $plugin->version = 2025041600;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.4';
-$plugin->supported = [311, 500];
+$plugin->supported = [401, 500];
